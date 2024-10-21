ea83761613551712ec60ccc3eb69739d3fd2da4c
University of Jyväskylä - Pedro Neto <pasoneto@gmail.com>
2024-10-21 09:57:53 +0300

    save every 1 epoch

--- job.sh
+++ job.sh
@@ -12,4 +12,4 @@ module load pytorch
 
 source /projappl/project_2009235/edge/bin/activate
 
-python3.9 train.py --batch_size 128 --epochs 2
+python3.9 train.py --batch_size 128 --epochs 2 --save_interval 1
