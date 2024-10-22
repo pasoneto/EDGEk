22e078ec78c49fe4edf643323d39b6bb92d9e83d
University of Jyväskylä - Pedro Neto <pasoneto@gmail.com>
2024-10-22 19:55:33 +0300

    job test

--- job_test.sh
+++ job_test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --account=project_2009235
 #SBATCH --partition=gpu
-#SBATCH --nodes=4
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
-#SBATCH --time=36:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --time=0:20:00
 #SBATCH --gres=gpu:v100:1
 
 module load pytorch
 
 source /projappl/project_2009235/edge/bin/activate
 
-python3.9 train.py --batch_size 128 --epochs 20000 --save_interval 1000
+python3.9 train.py --batch_size 128 --epochs 2 --save_interval 1
