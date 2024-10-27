faf216d44443d57980c2241a94c84d40703b5300
University of Jyväskylä - Pedro Neto <pasoneto@gmail.com>
2024-10-27 18:27:32 +0200

    commit befor run of position prediction (not angle)

--- vis.py
+++ vis.py
@@ -402,10 +402,10 @@ def visu(positions, sr):
 #f = "/Users/pdealcan/Documents/github/edge_redo/EDGEk/data/test/motions_sliced/CLIO_Zorbas_poses_slice14.pkl"
 #a = np.load(f, allow_pickle=True)
 #visu(a, 30)
-#f = "/Users/pdealcan/Downloads/6900_0_ANDREAS_Tsesto_Zonaradiko_poses_slice7.pkl"
+#f = "/Users/pdealcan/Downloads/1_0_CLIO_Pastirske_poses_slice7.pkl"
 #a = np.load(f, allow_pickle=True)
 #[print(b) for b in a]
 #positions, _ = smplToPosition(a['q'], a['pos'], 1, aist = False)
 #positions = positions[0]
-#positions = a['full_pose']
+#positions = a['full_pose'].reshape(300, 24, 3)
 #visu(positions, 30)
